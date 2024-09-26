f0652ecf47e15d39a7b519bfd13ef16b4a8e9d94
NotTechTonic <143181499+NotTechTonic@users.noreply.github.com>
2024-09-26 17:15:34 +0530

    Update groq.py

--- INTELLIGENCE/REACTION/LINGUISTICS/groq.py
+++ INTELLIGENCE/REACTION/LINGUISTICS/groq.py
@@ -3,6 +3,20 @@ import os
 from dotenv import load_dotenv; load_dotenv()
 
 def generate_response(query: str, system_prompt: str = "You are a humorous AI, use emojis and sarcasm in your responses.") -> object:
+  """
+    Generates a response from the groq-llama3-70b-8192 model based on the given query.
+
+    Parameters:
+    - query (str): The input text to which the model will respond in a string format.
+    - system_prompt (str, optional): The system prompt to use for the model. Defaults to "You are a humorous AI, use emojis and sarcasm in your responses.".    
+   
+    Returns:
+    - object: response object
+
+    This function initializes a session with the Groq client,
+    and creates a chat completion with the model and user's prompt along with the system prompt. Returns the response object at last.
+  """
+  
   groq_client = Groq(api_key=os.environ.get('GROQ_AI'))
 
   response = groq_client.chat.completions.create(

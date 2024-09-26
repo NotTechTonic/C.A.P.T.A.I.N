a6aa95760144cf3d4ad34f9ec12740edfd46cbf1
NotTechTonic <143181499+NotTechTonic@users.noreply.github.com>
2024-09-26 17:15:18 +0530

    Update text_to_speech.py

--- SPEECH_MODULES/SPEECH_SYNTHESIS/text_to_speech.py
+++ SPEECH_MODULES/SPEECH_SYNTHESIS/text_to_speech.py
@@ -5,5 +5,17 @@ engine = pyttsx3.init()
 engine.setProperty('voice', 1)
 
 def speak(text: str) -> None:
+  """
+    This function generates speech from text at lightning speed.
+
+    Parameters:
+    - query (str): The input text to which the model will respond in a string format.
+   
+    Returns:
+    - None: nothing
+
+    This function simply convertes text into speech offline really fast.
+  """
+
   engine.say(emoji.replace_emoji(text, replace=''))
   engine.runAndWait()
